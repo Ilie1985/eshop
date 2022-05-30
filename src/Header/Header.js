561c403942b5b3954f032e445c64e103ed5f1a1c
Marian Ilie <ilie.marian32@yahoo.com>
2022-05-30 11:57:22 +0100

    header comp css

---
+++ src/Header/Header.js
@@ -0,0 +1,47 @@
+import "../Header/header.css";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+import StorefrontIcon from "@mui/icons-material/Storefront";
+import SearchIcon from "@mui/icons-material/Search";
+
+//divide the header in 3(logo,searchbar,nav)
+//divide nav in 3 (item)
+//bring in basket and store icon from material UI
+//create an input type text with a className
+//bring in search incon  from material ui for search bar
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header_logo">
+        <StorefrontIcon className="header_logoImage" fontSize="large" />
+        <h2 className="header_logoTitle">eShop</h2>
+      </div>
+      {/* ==============================================================*/}
+
+      <div className="header_searchBar">
+        <input type="text" className="header_searchInput"></input>
+        <SearchIcon className="header_searchIcon" />
+      </div>
+
+      {/*==================================================================  */}
+
+      <div className="header_nav">
+        <div className="nav_item">
+          <span className="nav_itemLineOne">Hello Guest</span>
+          <span className="nav_itemLineTwo">Sign In</span>
+        </div>
+        <div className="nav_item">
+          <span className="nav_itemLineOne">Your</span>
+          <span className="nav_itemLineTwo">Shop</span>
+        </div>
+        <div className="nav_item">
+          <ShoppingBasketIcon className="nav_itemBasket"/>
+          <span className="nav_basketCount">0</span>
+        </div>
+      </div>
+      {/* =========================================================== */}
+    </div>
+  );
+};
+
+export default Header;
